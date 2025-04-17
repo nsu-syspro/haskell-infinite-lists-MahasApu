a7da7ba763ae1817a38bce56df162cf0d2499840
mahasapu <maryvask0@yandex.ru>
2025-04-17 23:09:00 +0700

    add solution for the 3rd task

--- src/Task2.hs
+++ src/Task2.hs
@@ -18,9 +18,67 @@ instance Foldable Stream where
   foldMap :: Monoid m => (a -> m) -> Stream a -> m
   foldMap f (Stream a s) = f a <> foldMap f s
 
+-- >>> fmap (* 2) $ fromList undefined [1..]
+-- [2,4,6,8,10,12,14,16,18,20]
+-- >>> fromList 2 []
+-- [2,2,2,2,2,2,2,2,2,2]
+--
 instance Functor Stream where
   fmap :: (a -> b) -> Stream a -> Stream b
   fmap f (Stream a s) = Stream (f a) (fmap f s)
+
+
+instance Applicative Stream where
+  pure :: a -> Stream a
+  pure x = Stream x $ pure x
+  (<*>) :: Stream (a -> b) -> Stream a -> Stream b
+  Stream f fs <*> Stream x xs = Stream (f x) (fs <*> xs)
+
+-- >>> 2 *:: fromList undefined [1..]
+-- [2,4,6,8,10,12,14,16,18,20]
+--
+infixl 7 *::
+(*::) :: Num a => a -> Stream a -> Stream a
+(*::) a  = fmap (* a) 
+
+
+-- >>> :t fromInteger
+-- fromInteger :: Num a => Integer -> a
+-- >>> :t fromList 0 . (: [])
+-- fromList 0 . (: []) :: Num a => a -> Stream a
+-- >>> :t fromList 0 . pure
+-- fromList 0 . pure :: Num a => a -> Stream a
+--
+instance Num a => Num (Stream a) where
+  (+) :: Num a => Stream a -> Stream a -> Stream a
+  (+) s1 s2 = (+) <$> s1 <*> s2
+
+  (*) :: Num a => Stream a -> Stream a -> Stream a
+  (*) (Stream x xs) s@(Stream y ys) = Stream (x * y) (x *:: ys + xs * s)
+
+  signum :: Num a => Stream a -> Stream a
+  signum = fmap signum
+
+  fromInteger :: Num a => Integer -> Stream a
+  fromInteger =  fromList 0 . pure . fromInteger
+
+  negate :: Num a => Stream a -> Stream a
+  negate = fmap negate
+  
+  abs :: Num a => Stream a -> Stream a
+  abs = fmap abs
+
+
+instance Fractional a => Fractional (Stream a) where
+  fromRational :: Fractional a => Rational -> Stream a
+  fromRational = fromList 0 . pure . fromRational
+
+  (/) :: Fractional a => Stream a -> Stream a -> Stream a
+  (/) (Stream x xs) s@(Stream y ys) = Stream factor rational 
+    where
+      factor = x / y
+      rational = (xs - factor *:: ys) / s
+
   
 -- | Converts given list into stream
 --
